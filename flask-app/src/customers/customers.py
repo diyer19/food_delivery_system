603448172bceac05a74876fadc0c307bb9157603
aneekmahajan <aneek.mahajan@icloud.com>
2023-04-18 17:42:49 -0400

    total price

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -288,7 +288,7 @@ def get_restaurants():
 @customers.route('/restaurants/<restaurant_name>', methods=['GET'])
 def get_menu_items(restaurant_name):
     cursor = db.get_db().cursor()
-    insert_stmt = "select concat('$', price, ' - ', item_name) as label, concat('$', price, ' - ', item_name) as value from Menu_Item JOIN Restaurant R on Menu_Item.restaurant_id = R.restaurant_id where restaurant_name="
+    insert_stmt = "select menu_item_id as value, item_name as label from Menu_Item JOIN Restaurant R on Menu_Item.restaurant_id = R.restaurant_id where restaurant_name="
     insert_stmt += "'{0}'".format(restaurant_name)
     cursor.execute(insert_stmt)
 
@@ -310,7 +310,21 @@ def get_menu_items(restaurant_name):
 
     return jsonify(json_data)
 
+## Get all Menu Items from the DB
+@customers.route('/total/<menu_item_id>', methods=['GET'])
+def get_total(menu_item_id):
+    cursor = db.get_db().cursor()
+    insert_stmt = "SELECT concat('$', price) AS price_1 FROM Menu_Item WHERE menu_item_id = '" + menu_item_id + "'"
+    
+    cursor.execute(insert_stmt)
 
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
 
 ## Place a New Order
 @customers.route('/new_order', methods=['POST'])
@@ -321,26 +335,34 @@ def post_new_order():
 
     current_app.logger.info(req_data)
 
-
+    list1 = []
+    
     # extracting the variables 
     # this needs to match the widget input box names in Appsmith 
     # ex: 'product_name', 'product_description', 'product_price', etc 
-    first_name = req_data['customer_firstname']
-    last_name = req_data['customer_lastname']
-    phone_number = req_data['customer_phone']
-    email = req_data['customer_email']
+    menu_items = req_data['menu_items']
+
+    # menu_items = json.loads(menu_items)
+    
+    # new_list = [element.replace('\\', "") for element in menu_items]
+
+    # for each in new_list: 
+    #     print(each)
+    #     list1.append(each) 
 
     # constructing the query 
 
-    insert_stmt = 'insert into Order (first_name, last_name, phone_number, email) values ("'
-    insert_stmt += first_name + '", "'
-    insert_stmt += last_name + '", "'
-    insert_stmt += phone_number + '", "'
-    insert_stmt += email + '" )'
+    # insert_stmt = 'insert into Order (first_name, last_name, phone_number, email) values ("'
+    # insert_stmt += first_name + '", "'
+    # insert_stmt += last_name + '", "'
+    # insert_stmt += phone_number + '", "'
+    # insert_stmt += email + '" )'
 
 
-    # executing anad commiting the insert stmt 
-    cursor = db.get_db().cursor()
-    cursor.execute(insert_stmt)
-    #can't commit the cursor, have to commit the db 
-    db.get_db().commit()
+    # # executing anad commiting the insert stmt 
+    # cursor = db.get_db().cursor()
+    # cursor.execute(insert_stmt)
+    # #can't commit the cursor, have to commit the db 
+    # db.get_db().commit()
+
+    return list1
