0a2e29a2349a4475760b468da618da0a38301dae
aneekmahajan <aneek.mahajan@icloud.com>
2023-04-17 21:56:59 -0400

    fixed routes

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -21,24 +21,34 @@ def get_customers():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get customer detail for customer with particular userID
-@customers.route('/customers/<userID>', methods=['GET'])
-def get_customer(userID):
+# Get customer details for customer with a particular number
+@customers.route('/customers/<phone_number>', methods=['GET'])
+def get_customer_id(phone_number):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from customers where id = {0}'.format(userID))
+    #cursor.execute('select * from Customer where phone_number = "{0}"'.format(phone_number))
+    
+    select = "SELECT c.customer_id, c.first_name, c.last_name, c.email, c.phone_number, da.street_address AS delivery_street_address, "
+    select += "da.zip AS delivery_zip, da.city AS delivery_city, da.state AS delivery_state, pi.payment_id, pi.cc, pi.expiration, "
+    select += "pi.zip AS payment_zip, pi.cvv, ba.street_address AS billing_street_address, ba.zip AS billing_zip, ba.city AS billing_city, "
+    select += "ba.state AS billing_state FROM Customer c JOIN Delivery_Address da ON c.customer_id = da.customer_id "
+    select += "JOIN Payment_Info pi ON c.customer_id = pi.customer_id JOIN Billing_Address ba ON pi.payment_id = ba.payment_id AND  "
+    select += 'pi.customer_id = ba.customer_id WHERE c.phone_number = "{0}"'.format(phone_number)
+    
+
+    cursor.execute(select)
+
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
     for row in theData:
+        print(row)
         json_data.append(dict(zip(row_headers, row)))
     the_response = make_response(jsonify(json_data))
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
 
-
-
-# Post  Customer
+# Post Customer
 @customers.route('/new_customer', methods=['POST'])
 def post_new_customer():
     current_app.logger.info('Processing form data')
@@ -70,89 +80,27 @@ def post_new_customer():
     cursor.execute(insert_stmt)
     #can't commit the cursor, have to commit the db 
     db.get_db().commit()
-    return "Success"
 
-# Post Delivery Address 
-@customers.route('/new_delivery_address', methods=['POST'])
-def post_new_delivery_address():
-    current_app.logger.info('Processing form data')
-    req_data = request.get_json()
-    current_app.logger.info(req_data)
-
-    # extracting the variables 
-    # this needs to match the widget input box names in Appsmith 
-    # ex: 'product_name', 'product_description', 'product_price', etc 
+    # Delivery Address 
     street = req_data['delivery_street']
     city = req_data['delivery_city']
     state = req_data['delivery_state']
     zipcode = req_data['delivery_zip']
 
     # constructing the query 
-
     insert_stmt = 'insert into Delivery_Address (street_address, state, city, zip) values ("'
     insert_stmt += street + '", "'
     insert_stmt += state + '", "'
     insert_stmt += city + '", "'
     insert_stmt += zipcode + '" )'
 
-
     # executing and commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(insert_stmt)
     #can't commit the cursor, have to commit the db 
     db.get_db().commit()
-    return "Success"
 
-# Post Billing Address 
-@customers.route('/new_billing_address', methods=['POST'])
-def post_new_billing_address():
-    current_app.logger.info('Processing form data')
-    req_data = request.get_json()
-    current_app.logger.info(req_data)
-
-    # extracting the variables 
-    # this needs to match the widget input box names in Appsmith 
-    # ex: 'product_name', 'product_description', 'product_price', etc 
-    street = req_data['billing_street']
-    city = req_data['billing_city']
-    state = req_data['billing_state']
-    zipcode = req_data['billing_zip']
-
-    # Getting Max Values 
-    cursor = db.get_db().cursor()
-    cursor.execute("SELECT COALESCE(MAX(customer_id), 0) FROM Billing_Address")
-    result = cursor.fetchone()
-    max_customer_id = int(result[0]) + 1
-
-    cursor = db.get_db().cursor()
-    cursor.execute("SELECT COALESCE(MAX(payment_id), 0) FROM Billing_Address")
-    result = cursor.fetchone()
-    max_payment_id = int(result[0]) + 1
-
-    # Constructing insert statement
-    insert_stmt = 'INSERT INTO Billing_Address (payment_id, customer_id, street_address, state, city, zip) '
-    insert_stmt += 'VALUES ('+ str(max_payment_id)+ ', '
-    insert_stmt += str(max_customer_id)+ ', "'
-    insert_stmt += street + '", "'
-    insert_stmt += state + '", "'
-    insert_stmt += city + '", '
-    insert_stmt += zipcode + ')'
-
-    # executing anad commiting the insert stmt 
-    cursor = db.get_db().cursor()
-    cursor.execute(insert_stmt)
-    #can't commit the cursor, have to commit the db 
-    db.get_db().commit()
-    return "Success"
-
-# Post Payment Info 
-@customers.route('/new_payment_info', methods=['POST'])
-def post_new_payment_info():
-    current_app.logger.info('Processing form data')
-    req_data = request.get_json()
-    current_app.logger.info(req_data)
-
-    # extracting the variables 
+    # Payment 
     cc = req_data['payment_number']
     expiration = req_data['payment_expiration']
     zipcode = req_data['payment_zip']
@@ -183,4 +131,37 @@ def post_new_payment_info():
     cursor.execute(insert_stmt)
     #can't commit the cursor, have to commit the db 
     db.get_db().commit()
+
+    # Billing Address 
+    street = req_data['billing_street']
+    city = req_data['billing_city']
+    state = req_data['billing_state']
+    zipcode = req_data['billing_zip']
+
+    # Getting Max Values 
+    cursor = db.get_db().cursor()
+    cursor.execute("SELECT COALESCE(MAX(customer_id), 0) FROM Billing_Address")
+    result = cursor.fetchone()
+    max_customer_id = int(result[0]) + 1
+
+    cursor = db.get_db().cursor()
+    cursor.execute("SELECT COALESCE(MAX(payment_id), 0) FROM Billing_Address")
+    result = cursor.fetchone()
+    max_payment_id = int(result[0]) + 1
+
+    # Constructing insert statement
+    insert_stmt = 'INSERT INTO Billing_Address (payment_id, customer_id, street_address, state, city, zip) '
+    insert_stmt += 'VALUES ('+ str(max_payment_id)+ ', '
+    insert_stmt += str(max_customer_id)+ ', "'
+    insert_stmt += street + '", "'
+    insert_stmt += state + '", "'
+    insert_stmt += city + '", '
+    insert_stmt += zipcode + ')'
+
+    # executing anad commiting the insert stmt 
+    cursor = db.get_db().cursor()
+    cursor.execute(insert_stmt)
+    #can't commit the cursor, have to commit the db 
+    db.get_db().commit()
+
     return "Success"
