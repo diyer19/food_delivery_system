b30c575ebd48334673df302500aa16d0d93bb26c
benghz <ghouzibg@gmail.com>
2023-04-18 22:47:45 -0400

    new restaurant and delete restaurant

--- flask-app/src/restaurants/restaurants.py
+++ flask-app/src/restaurants/restaurants.py
@@ -1,60 +1,86 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
 
 restaurants = Blueprint('restaurants', __name__)
 
-# # Get all the products from the database
-# @products.route('/products', methods=['GET'])
-# def get_products():
-#     # get a cursor object from the database
-#     cursor = db.get_db().cursor()
 
-#     # use cursor to query the database for a list of products
-#     cursor.execute('SELECT id, product_code, product_name, list_price FROM products')
+# Post Restaurant
+@restaurants.route('/new_restaurant', methods=['POST'])
+def post_new_restaurant():
+    current_app.logger.info('Processing form data')
 
-#     # grab the column headers from the returned data
-#     column_headers = [x[0] for x in cursor.description]
+    req_data = request.get_json()
 
-#     # create an empty dictionary object to use in 
-#     # putting column headers together with data
-#     json_data = []
+    current_app.logger.info(req_data)
 
-#     # fetch all the data from the cursor
-#     theData = cursor.fetchall()
 
-#     # for each of the rows, zip the data elements together with
-#     # the column headers. 
-#     for row in theData:
-#         json_data.append(dict(zip(column_headers, row)))
+    # extracting the variables 
+    # this needs to match the widget input box names in Appsmith 
+    # ex: 'product_name', 'product_description', 'product_price', etc 
+    restaurant_name = req_data['restaurant_name']
+    phone_number = req_data['restaurant_phone']
+    street_address = req_data['restaurant_street']
+    city = req_data['restaurant_city']
+    state = req_data['restaurant_state']
+    zicode = req_data['restaurant_zip']
 
-#     return jsonify(json_data)
+    # constructing the query 
 
-# # get the top 5 products from the database
-# @products.route('/mostExpensive')
-# def get_most_pop_products():
-#     cursor = db.get_db().cursor()
-#     query = '''
-#         SELECT product_code, product_name, list_price, reorder_level
-#         FROM products
-#         ORDER BY list_price DESC
-#         LIMIT 5
-#     '''
-#     cursor.execute(query)
-#        # grab the column headers from the returned data
-#     column_headers = [x[0] for x in cursor.description]
+    insert_stmt = 'insert into Customer (restaurant_name, phone_number, street_address, city, state, zip) values ("'
+    insert_stmt += restaurant_name + '", "'
+    insert_stmt += phone_number + '", "'
+    insert_stmt += street_address + '", "'
+    insert_stmt += city + '", "'
+    insert_stmt += state + '", "'
+    insert_stmt += zicode + '" )'
 
-#     # create an empty dictionary object to use in 
-#     # putting column headers together with data
-#     json_data = []
 
-#     # fetch all the data from the cursor
-#     theData = cursor.fetchall()
+    # executing anad commiting the insert stmt 
+    cursor = db.get_db().cursor()
+    cursor.execute(insert_stmt)
+    #can't commit the cursor, have to commit the db 
+    db.get_db().commit()
 
-#     # for each of the rows, zip the data elements together with
-#     # the column headers. 
-#     for row in theData:
-#         json_data.append(dict(zip(column_headers, row)))
+    return "Success"
 
-#     return jsonify(json_data)
+
+# Delete Restaurant
+@restaurants.route('/new_restaurant', methods=['DELETE'])
+def delete_new_restaurant():
+    current_app.logger.info('Processing form data')
+
+    req_data = request.get_json()
+
+    current_app.logger.info(req_data)
+
+
+    # extracting the variables 
+    # this needs to match the widget input box names in Appsmith 
+    # ex: 'product_name', 'product_description', 'product_price', etc 
+    restaurant_name = req_data['restaurant_name']
+    phone_number = req_data['restaurant_phone']
+    street_address = req_data['restaurant_street']
+    city = req_data['restaurant_city']
+    state = req_data['restaurant_state']
+    zicode = req_data['restaurant_zip']
+
+    # constructing the query 
+
+    delete_stmt = 'delete Customer (restaurant_name, phone_number, street_address, city, state, zip) values ("'
+    delete_stmt += restaurant_name + '", "'
+    delete_stmt += phone_number + '", "'
+    delete_stmt += street_address + '", "'
+    delete_stmt += city + '", "'
+    delete_stmt += state + '", "'
+    delete_stmt += zicode + '" )'
+
+
+    # executing anad commiting the insert stmt 
+    cursor = db.get_db().cursor()
+    cursor.execute(delete_stmt)
+    #can't commit the cursor, have to commit the db 
+    db.get_db().commit()
+
+    return "Success"
