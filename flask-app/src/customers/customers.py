5cae96b4fba27585a4061a2df5b010238a0e7df1
patten-an <patten.an@northeastern.edu>
2023-04-19 17:14:40 -0400

    comments

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -6,7 +6,7 @@ import random
 
 customers = Blueprint('customers', __name__)
 
-# 1. Get all customers from the DB
+# 1. Get all Customers (GET)
 @customers.route('/customers', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
@@ -22,12 +22,12 @@ def get_customers():
     the_response.mimetype = 'application/json'
     return the_response
 
-# 2. Get customer details for customer with a particular number
+# 2. Get details of a specific customer
 @customers.route('/customers/<phone_number>', methods=['GET'])
 def get_customer_id(phone_number):
     cursor = db.get_db().cursor()
-    #cursor.execute('select * from Customer where phone_number = "{0}"'.format(phone_number))
-    
+
+    # query    
     select = "SELECT c.customer_id, c.first_name, c.last_name, c.email, c.phone_number, da.street_address AS delivery_street_address, "
     select += "da.zip AS delivery_zip, da.city AS delivery_city, da.state AS delivery_state, pi.payment_id, pi.cc, pi.expiration, "
     select += "pi.zip AS payment_zip, pi.cvv, ba.street_address AS billing_street_address, ba.zip AS billing_zip, ba.city AS billing_city, "
@@ -49,7 +49,7 @@ def get_customer_id(phone_number):
     the_response.mimetype = 'application/json'
     return the_response
 
-# 3. Post Customer
+# 3. Create a new customer (POST)
 @customers.route('/new_customer', methods=['POST'])
 def post_new_customer():
     current_app.logger.info('Processing form data')
@@ -58,50 +58,43 @@ def post_new_customer():
 
     current_app.logger.info(req_data)
 
-
-    # extracting the variables 
-    # this needs to match the widget input box names in Appsmith 
-    # ex: 'product_name', 'product_description', 'product_price', etc 
+    # extract variables (customer info)
     first_name = req_data['customer_firstname']
     last_name = req_data['customer_lastname']
     phone_number = req_data['customer_phone']
     email = req_data['customer_email']
 
-    # constructing the query 
-
+    # query
     insert_stmt = 'insert into Customer (first_name, last_name, phone_number, email) values ("'
     insert_stmt += first_name + '", "'
     insert_stmt += last_name + '", "'
     insert_stmt += phone_number + '", "'
     insert_stmt += email + '" )'
 
-
-    # executing anad commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(insert_stmt)
-    #can't commit the cursor, have to commit the db 
+
     db.get_db().commit()
 
-    # Delivery Address 
+    # extract variables (delivery address)
     street = req_data['delivery_street']
     city = req_data['delivery_city']
     state = req_data['delivery_state']
     zipcode = req_data['delivery_zip']
 
-    # constructing the query 
+    # query
     insert_stmt = 'insert into Delivery_Address (street_address, state, city, zip) values ("'
     insert_stmt += street + '", "'
     insert_stmt += state + '", "'
     insert_stmt += city + '", "'
     insert_stmt += zipcode + '" )'
 
-    # executing and commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(insert_stmt)
-    #can't commit the cursor, have to commit the db 
+
     db.get_db().commit()
 
-    # Payment 
+    # extract variables (payment info) 
     cc = req_data['payment_number']
     expiration = req_data['payment_expiration']
     zipcode = req_data['payment_zip']
@@ -118,7 +111,7 @@ def post_new_customer():
     result = cursor.fetchone()
     max_payment_id = int(result[0]) + 1
 
-    # Constructing insert statement
+    # query
     insert_stmt = 'INSERT INTO Payment_Info (payment_id, customer_id, cc, expiration, zip, cvv) '
     insert_stmt += 'VALUES (' + str(max_payment_id) + ', '
     insert_stmt += str(max_customer_id) + ', "'
@@ -127,13 +120,12 @@ def post_new_customer():
     insert_stmt += zipcode + ', '
     insert_stmt += cvv + ')'
 
-    # executing anad commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(insert_stmt)
-    #can't commit the cursor, have to commit the db 
+    
     db.get_db().commit()
 
-    # Billing Address 
+    # extract variables (billing address)
     street = req_data['billing_street']
     city = req_data['billing_city']
     state = req_data['billing_state']
@@ -150,7 +142,7 @@ def post_new_customer():
     result = cursor.fetchone()
     max_payment_id = int(result[0]) + 1
 
-    # Constructing insert statement
+    # query
     insert_stmt = 'INSERT INTO Billing_Address (payment_id, customer_id, street_address, state, city, zip) '
     insert_stmt += 'VALUES ('+ str(max_payment_id)+ ', '
     insert_stmt += str(max_customer_id)+ ', "'
@@ -159,70 +151,69 @@ def post_new_customer():
     insert_stmt += city + '", '
     insert_stmt += zipcode + ')'
 
-    # executing anad commiting the insert stmt 
+    
     cursor = db.get_db().cursor()
     cursor.execute(insert_stmt)
-    #can't commit the cursor, have to commit the db 
+    
     db.get_db().commit()
 
     return "Success"
 
-# 4. update customer's billing_address
+# 4. Update a specific customer's billing address (PUT)
 @customers.route('/customers/<phone_number>/billing_address', methods=['PUT'])
 def update_billing_address(phone_number):
     cursor = db.get_db().cursor()
 
-    # getting update data
+    
     current_app.logger.info('Processing form data')
     req_data = request.get_json()
     current_app.logger.info(req_data)
 
 
-    # extracting the variables 
+    # extract variables
     street = str(req_data['billing_street'])
     city = str(req_data['billing_city'])
     state = str(req_data['billing_state'])
     zipcode = str(req_data['billing_zip'])
 
+    # query
     update = "UPDATE Billing_Address"
     update += " SET street_address = '" + street + "', city = '" + city + "', state = '" + state + "', zip = '" + zipcode + "'"
     update += " WHERE customer_id = (SELECT customer_id FROM Customer WHERE phone_number = '{0}'".format(phone_number)
     update += ")"
 
-    # executing and commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(update)
-    #can't commit the cursor, have to commit the db 
+
     db.get_db().commit()
 
     return "success"
 
-# 5. update customer's delivery_address
+# 5. Udate a specific customer's delivery address (PUT)
 @customers.route('/customers/<phone_number>/delivery_address', methods=['PUT'])
 def update_delivery_address(phone_number):
     cursor = db.get_db().cursor()
 
-    # getting update data
     current_app.logger.info('Processing form data')
     req_data = request.get_json()
     current_app.logger.info(req_data)
 
 
-    # extracting the variables 
+    # extract variables 
     street = str(req_data['delivery_street'])
     city = str(req_data['delivery_city'])
     state = str(req_data['delivery_state'])
     zipcode = str(req_data['delivery_zip'])
 
+    # query
     update = "UPDATE Delivery_Address"
     update += " SET street_address = '" + street + "', city = '" + city + "', state = '" + state + "', zip = '" + zipcode + "'"
     update += " WHERE customer_id = (SELECT customer_id FROM Customer WHERE phone_number = '{0}'".format(phone_number)
     update += ")"
 
-    # executing and commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(update)
-    #can't commit the cursor, have to commit the db 
+    
     db.get_db().commit()
 
     return "success"
@@ -230,91 +221,84 @@ def update_delivery_address(phone_number):
 
 
 
-# 6. update customer's payment info
+# 6. Update a specific customer's payment info (PUT)
 @customers.route('/customers/<phone_number>/payment_info', methods=['PUT'])
 def update_payment_info(phone_number):
     cursor = db.get_db().cursor()
 
-    # getting update data
     current_app.logger.info('Processing form data')
     req_data = request.get_json()
     current_app.logger.info(req_data)
 
 
-    # extracting the variables 
+    # extract variables 
     cc = str(req_data['payment_number'])
     expiration = str(req_data['payment_expiration'])
     zipcode = str(req_data['payment_zip'])
     cvv = str(req_data['payment_cvv'])
 
+    # query
     update = "UPDATE Payment_Info"
     update += " SET cc = '" + cc + "', expiration = '" + expiration + "', zip = '" + zipcode + "', cvv = '" + cvv + "'"
     update += " WHERE customer_id = (SELECT customer_id FROM Customer WHERE phone_number = '{0}'".format(phone_number)
     update += ")"
 
-    # executing and commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(update)
-    #can't commit the cursor, have to commit the db 
+    
     db.get_db().commit()
 
     return "success"
 
 
-## 7. Get all Restaurants from the DB
+## 7. Get all Restaurants (GET)
 @customers.route('/restaurants', methods=['GET'])
 def get_restaurants():
     cursor = db.get_db().cursor()
+
+    # execute query
     cursor.execute('select distinct restaurant_name as label, restaurant_name as value from Restaurant')
 
-    # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
 
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
     json_data = []
 
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
+    
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
 
 
-## 8. Get all Menu Items from the DB
+## 8. Get all menu items from a specific restaurant (GET)
 @customers.route('/restaurants/<restaurant_name>', methods=['GET'])
 def get_menu_items(restaurant_name):
     cursor = db.get_db().cursor()
+
+    # query
     insert_stmt = "select menu_item_id as value, item_name as label from Menu_Item JOIN Restaurant R on Menu_Item.restaurant_id = R.restaurant_id where restaurant_name="
     insert_stmt += "'{0}'".format(restaurant_name)
+
     cursor.execute(insert_stmt)
 
-
-    # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
 
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
     json_data = []
 
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
 
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
 
-## 9. Get all Menu Items from the DB
+## 9. Get all Menu Items (GET)
 @customers.route('/total/<menu_item_id>', methods=['GET'])
 def get_total(menu_item_id):
     cursor = db.get_db().cursor()
+
+    # query
     insert_stmt = "SELECT concat('$', price) AS price_1 FROM Menu_Item WHERE menu_item_id = '" + menu_item_id + "'"
     
     cursor.execute(insert_stmt)
@@ -327,7 +311,7 @@ def get_total(menu_item_id):
 
     return jsonify(json_data)
 
-## 10. Place a New Order
+## 10. Place a New Order (POST)
 @customers.route('/new_order', methods=['POST'])
 def post_new_order():
     current_app.logger.info('Processing form data')
@@ -336,9 +320,7 @@ def post_new_order():
 
     current_app.logger.info(req_data)
     
-    # extracting the variables 
-    # this needs to match the widget input box names in Appsmith 
-    # ex: 'product_name', 'product_description', 'product_price', etc 
+    # extract variables 
     restaurant_name = str(req_data['Order_restaurant'])
     menu_item = str(req_data['menu_items1'])
     phone_number = str(req_data['customer_phone'])
@@ -348,8 +330,8 @@ def post_new_order():
     # simulating choosing a driver 
     driver_id = str(random.randint(1, 20))
     earnings = str(random.randint(10, 50))
-    # constructing the query 
-
+    
+    # query 
     order_stmt = "INSERT INTO Order_Table (customer_id, restaurant_id, driver_id, order_total, earnings) VALUES "
     order_stmt += "((SELECT customer_id FROM Customer WHERE phone_number = '"+phone_number+"'), "
     order_stmt += "(SELECT restaurant_id FROM Restaurant WHERE restaurant_name = '"+restaurant_name+"'), "
@@ -357,86 +339,71 @@ def post_new_order():
     order_stmt += order_total 
     order_stmt += ", "+earnings+");" #should be rand int     
 
-    # executing anad commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(order_stmt)
-    #can't commit the cursor, have to commit the db 
+    
     db.get_db().commit()
 
+    # query (insert into MenuItem_Order)
     insert = "INSERT INTO MenuItem_Order (order_id, restaurant_id, menu_item_id, customer_id, driver_id) VALUES ("
     insert += "(SELECT MAX(order_id)  FROM Order_Table),"
     insert +="(SELECT restaurant_id FROM Restaurant WHERE restaurant_name = '"+restaurant_name+"'),"
     insert += menu_item+","
     insert += "(SELECT customer_id FROM Customer WHERE phone_number = '"+phone_number+"'), ("+driver_id+"));"
 
-    # executing anad commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(insert)
-    #can't commit the cursor, have to commit the db 
+    
     db.get_db().commit()
 
     return  "success"
 
-## 11. Get Orders
+## 11. Get Orders for a specific customer (GET)
 @customers.route('/orders/<phone_number>', methods=['GET'])
 def get_orders(phone_number):
     cursor = db.get_db().cursor()
 
+    # query
     select = "SELECT * FROM Order_Table JOIN Customer ON Order_Table.customer_id = Customer.customer_id"
     select += " WHERE Customer.phone_number = '" + phone_number + "';"
 
-
     cursor.execute(select)
 
-
-    # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
 
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
     json_data = []
 
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
 
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
 
-## 12. Get Orders
+## 12. Get Orders for a specific customer (for dropdown for choosing items to order) (GET)
 @customers.route('/order_id/<phone_number>', methods=['GET'])
 def get_orders_id(phone_number):
     cursor = db.get_db().cursor()
 
+    # query
     select = "SELECT order_id as label, order_id as value FROM Order_Table JOIN Customer ON Order_Table.customer_id = Customer.customer_id"
     select += " WHERE Customer.phone_number = '" + phone_number + "';"
 
-
     cursor.execute(select)
 
-
-    # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
 
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
     json_data = []
 
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
 
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
 
 
-## Cancel an Order
+## Cancel an Order (DELETE)
 @customers.route('/cancel_order', methods=['DELETE'])
 def delete_order():
     current_app.logger.info('Processing form data')
@@ -445,19 +412,15 @@ def delete_order():
 
     current_app.logger.info(req_data)
     
-    # extracting the variables 
-    # this needs to match the widget input box names in Appsmith 
-    # ex: 'product_name', 'product_description', 'product_price', etc 
+    # extract variables
     order_cancel = str(req_data['menu_items1'])
 
-    # constructing the query 
-
+    # query 
     cancel_stmt = "DELETE FROM Order_Table WHERE order_id = "+order_cancel
 
-    # executing anad commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(cancel_stmt)
-    #can't commit the cursor, have to commit the db 
+    
     db.get_db().commit()
 
     return  "success"
--- flask-app/src/restaurants/restaurants.py
+++ flask-app/src/restaurants/restaurants.py
@@ -7,7 +7,7 @@ import random
 restaurants = Blueprint('restaurants', __name__)
 
 
-# 1. Post Restaurant (WORKING)
+# 1. Create a new Restaurant (POST)
 @restaurants.route('/new_restaurant', methods=['POST'])
 def post_new_restaurant():
     current_app.logger.info('Processing form data')
@@ -16,10 +16,7 @@ def post_new_restaurant():
 
     current_app.logger.info(req_data)
 
-
-    # extracting the variables 
-    # this needs to match the widget input box names in Appsmith 
-    # ex: 'product_name', 'product_description', 'product_price', etc 
+    # extract variables
     avg_delivery = str(random.randint(1, 20))
     restaurant_name = req_data['restaurant_name']
     phone_number = req_data['restaurant_phone']
@@ -28,8 +25,7 @@ def post_new_restaurant():
     state = req_data['restaurant_state']
     zicode = str(req_data['restaurant_zip'])
 
-    # constructing the query 
-
+    # query
     insert_stmt = 'insert into Restaurant (restaurant_name, phone_number, street_address, city, state, avg_delivery_time, zip) values ("'
     insert_stmt += restaurant_name + '", "'
     insert_stmt += phone_number + '", "'
@@ -39,133 +35,111 @@ def post_new_restaurant():
     insert_stmt += avg_delivery + '", "'
     insert_stmt += zicode + '" )'
 
-
-    # executing anad commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(insert_stmt)
-    #can't commit the cursor, have to commit the db 
+    
     db.get_db().commit()
 
     return "Success"
 
 
-## 2. Get all Menu Items (WORKING)
+## 2. Get all the Menu Items from a specific restaurant (GET)
 @restaurants.route('/restaurants/<restaurant_name>', methods=['GET'])
 def get_menu_items_restaurant(restaurant_name):
     cursor = db.get_db().cursor()
+
+    #query
     insert_stmt = "select menu_item_id as value, item_name as label from Menu_Item JOIN Restaurant R on Menu_Item.restaurant_id = R.restaurant_id where restaurant_name="
     insert_stmt += "'{0}'".format(restaurant_name)
     cursor.execute(insert_stmt)
 
 
-    # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
 
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
     json_data = []
 
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
 
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
 
 
-## 3. Update MenuItem (WORKING)
+## 3. Update a Restaurant's Menu Item (PUT)
 @restaurants.route('/restaurant/<restaurant_name1>/<select_menu_item>', methods=['PUT'])
 def update_menu_item(restaurant_name1, select_menu_item):
     cursor = db.get_db().cursor()
 
-    # getting update data
     current_app.logger.info('Processing form data')
     req_data = request.get_json()
     current_app.logger.info(req_data)
 
 
-    # extracting the variables 
+    # extract variables
     menu_item_id = str(req_data['select_menu_item'])
     item_name = str(req_data['item_name'])
     descrip = str(req_data['item_description'])
     price = str(req_data['item_price'])
 
-    
+    # query
     update = "UPDATE Menu_Item SET item_name = '"+item_name+"', descrip = '"+descrip+"',"
     update += "price = '"+price+"' WHERE menu_item_id = "+menu_item_id+";"
 
-    # executing and commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(update)
-    #can't commit the cursor, have to commit the db 
+
     db.get_db().commit()
 
     return "success"
 
-## 4. delete restaurant
+## 4. Delete a Restaurant (DELETE)
 @restaurants.route('/delete_restaurant', methods=['DELETE'])
 def delete_restaurant():
     current_app.logger.info('Processing form data')
     req_data = request.get_json()
     current_app.logger.info(req_data)
     
-
+    #extract data
     restaurant_cancel = str(req_data['restaurant_name2'])
 
-    # constructing the query
-    # 
-    # DELETE FROM Order_Table WHERE restaurant_id IN (SELECT restaurant_id FROM Restaurant WHERE restaurant_name = 'Vimbo');
-
-    # DELETE FROM Restaurant WHERE restaurant_name = 'Vimbo';
-
+    # query (delete orders of this restaurant)
     cancel_stmt = "DELETE FROM Order_Table WHERE restaurant_id IN (SELECT restaurant_id FROM Restaurant WHERE restaurant_name = '"+restaurant_cancel+"');"
 
-    # executing anad commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(cancel_stmt)
-    #can't commit the cursor, have to commit the db s
+
     db.get_db().commit()
 
+    # delete the restaurant
     cancel_stmt1 = "DELETE FROM Restaurant WHERE restaurant_name = '"+restaurant_cancel+"';"
 
-    # executing anad commiting the insert stmt 
     cursor = db.get_db().cursor()
     cursor.execute(cancel_stmt1)
-    #can't commit the cursor, have to commit the db 
+    
     db.get_db().commit()
 
     return  "success"
 
 
-## 5. get restaurant orders
+## 5. Get all the orders of a restaurant (GET)
 @restaurants.route('/orders/<restaurant_name>', methods=['GET'])
 def get_restaurant_orders(restaurant_name):
    
    cursor = db.get_db().cursor()
 
+    # query
    insert_stmt = "SELECT order_id, order_total, time_placed, time_delivered, time_picked_up FROM Order_Table WHERE restaurant_id = (SELECT restaurant_id FROM Restaurant WHERE restaurant_name = "
    insert_stmt += "'{0}'".format(restaurant_name) + ")"
 
    cursor.execute(insert_stmt)
 
-   # grab the column headers from the returned data
    column_headers = [x[0] for x in cursor.description]
 
-
-   # create an empty dictionary object to use in
-   # putting column headers together with data
    json_data = []
 
-
-   # fetch all the data from the cursor
    theData = cursor.fetchall()
 
-
-   # for each of the rows, zip the data elements together with
-   # the column headers.
    for row in theData:
        json_data.append(dict(zip(column_headers, row)))
 
@@ -173,47 +147,37 @@ def get_restaurant_orders(restaurant_name):
    return jsonify(json_data)
 
 
-## 6. get restaurant reviews
+## 6. Get all the reviews of a restaurant (GET)
 @restaurants.route('/restaurant_reviews/<restaurant_name>', methods=['GET'])
 def get_restaurant_reviews(restaurant_name):
    
    cursor = db.get_db().cursor()
 
+    # query
    insert_stmt = "SELECT score, review, review date FROM Restaurant_Review WHERE restaurant_id = (SELECT restaurant_id FROM Restaurant WHERE restaurant_name ="
    insert_stmt += "'{0}'".format(restaurant_name) + ")"
 
    cursor.execute(insert_stmt)
 
-   # grab the column headers from the returned data
    column_headers = [x[0] for x in cursor.description]
 
-
-   # create an empty dictionary object to use in
-   # putting column headers together with data
    json_data = []
 
-
-   # fetch all the data from the cursor
    theData = cursor.fetchall()
 
-
-   # for each of the rows, zip the data elements together with
-   # the column headers.
    for row in theData:
        json_data.append(dict(zip(column_headers, row)))
-
 
    return jsonify(json_data)
 
 
-# 7: get average restaurant score
+# 7: Get the average review score of a restaurant (GET)
 @restaurants.route('/avg_score/<restaurant_name>', methods=['GET'])
 def get_avg_score(restaurant_name):
 
     cursor = db.get_db().cursor()
 
-    #insert_stmt = "select avg(score) from Restaurant_Review join Restaurant R on Restaurant_Review.restaurant_id = R.restaurant_id where restaurant_name='" + restaurant_name + "'"
-    
+    #query
     insert_stmt = "select avg(score) from Restaurant_Review join Restaurant R on Restaurant_Review.restaurant_id = "
     insert_stmt += "R.restaurant_id where restaurant_name='"+restaurant_name+"';"
 
@@ -227,12 +191,13 @@ def get_avg_score(restaurant_name):
 
     return jsonify(json_data)
 
-# 8: get a restaurant's total earnings
+# 8: Get the total earnings of a restaurant (GET)
 @restaurants.route('/restaurant_earnings/<restaurant_name>', methods=['GET'])
 def get_total_earnings(restaurant_name):
 
     cursor = db.get_db().cursor()
 
+    # query
     insert_stmt = "select sum(earnings) from Order_Table join Restaurant R on Order_Table.restaurant_id = R.restaurant_id where restaurant_name='" + restaurant_name + "'"
     
     cursor.execute(insert_stmt)
